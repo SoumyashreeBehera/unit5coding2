1187ae6ff0865593a20a8901ff098400512fcb3a
Soumya <soumyabb2015@gmail.com>
2021-11-08 16:47:43 +0530

    authorization on student controller

--- src/controllers/lectures.controller.js
+++ src/controllers/lectures.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const authenticate = require("../middlewares/authenticate");
-const authorize = require("../middlewares/authorize");
+const { authorize, authorizeOne } = require("../middlewares/authorize");
 const router = express.Router();
 const Lecture = require("../models/lectures.model");
 
@@ -17,6 +17,19 @@ router.post(
     }
   }
 );
+
+router.patch("/:id", authenticate, authorizeOne(), async function (req, res) {
+  try {
+    let lecture = await Lecture.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .lean()
+      .exec();
+    return res.status(200).json({ lecture });
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+});
 
 router.get("", async function (req, res) {
   try {
--- src/middlewares/authorize.js
+++ src/middlewares/authorize.js
@@ -1,3 +1,6 @@
+const Lecture = require("../models/lectures.model");
+const jwt = require("jsonwebtoken");
+
 const authorize = (given) => {
   return function (req, res, next) {
     const user = req.user;
@@ -7,4 +10,18 @@ const authorize = (given) => {
   };
 };
 
-module.exports = authorize;
+const authorizeOne = (given) => {
+  return async function (req, res, next) {
+    const lecture = await Lecture.findById(req.params.id).lean().exec();
+    console.log(lecture);
+    const user = req.user;
+    console.log(user);
+
+    if (user.roles == "admin") return next();
+    else if (user.roles == "instructor" && user._id == lecture.instructor)
+      return next();
+    return res.status(401).send("you are not permitted");
+  };
+};
+
+module.exports = { authorize, authorizeOne };
